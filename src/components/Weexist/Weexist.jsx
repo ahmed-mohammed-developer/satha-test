c806f884112ff2a8e877bb5cc3ba8a87726579e6
ahmed <ahmed.zaakan@gmail.com>
2024-11-20 19:11:37 +0300

    ass new section

--- src/components/Weexist/Weexist.jsx
+++ src/components/Weexist/Weexist.jsx
@@ -6,6 +6,13 @@ function Weexist() {
     <div className="background-section">
     <div className="overlay">
     <div>Weexist</div>
+    <div>Weexist</div>
+    <div>Weexist</div>
+    <div>Weexist</div>
+    <div>Weexist</div>
+    <div>Weexist</div>
+    <div>Weexist</div>
+
     </div>
   </div>
   
